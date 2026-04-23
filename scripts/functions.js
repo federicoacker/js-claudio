fe9a4d89ea4028ab250ebb1559e7aa0316b90da4
federicoacker <acker.federico@gmail.com>
2026-04-23 14:34:06 +0200

    Riuscito ad ottenere una risposta

--- scripts/functions.js
+++ scripts/functions.js
@@ -8,5 +8,30 @@ function askClaude(message){
         content: message
     }
 
-    history.pushState(userMessage);
+    history.push(userMessage);
+    const body = JSON.stringify({
+            "max_tokens": 1024,
+            "model": CLAUDE_MODEL,
+            "messages": history
+        });
+
+    let claudeResponse = fetch(CLAUDE_API_URL, 
+    {
+        method:"POST",
+        headers: claudeHeader,
+        body: body
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data =>
+    {
+        console.log(data);
+    }
+    )
+    .catch(error => {
+        console.log(error);
+    })
 }
+
+askClaude("Ciao Claude");
