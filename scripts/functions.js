263c08ee2a03be8da32633818cc264e6dd8baafc
federicoacker <acker.federico@gmail.com>
2026-04-23 15:34:21 +0200

    Creati riferimenti agli elementi che mi servono della pagina

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,10 @@
+const dom = {
+    userMessageTemplate = document.querySelector("#user-message-template"),
+    aiMessageTemplate = document.querySelector("#ai-message-template"),
+    inputFormEl = document.querySelector("#user-input-form"),
+    inputEl = document.querySelector("#user-message-input"),
+    chatContainerEl = document.querySelector(".chat-container")
+}
 
 const history = [];
 
