ddc3b5254e79c0eb64e8b7020ccfe9f9c20b48e0
federicoacker <acker.federico@gmail.com>
2026-04-23 15:45:22 +0200

    Creata funzione che fa la stessa cosa anche per la AI

--- scripts/functions.js
+++ scripts/functions.js
@@ -14,13 +14,18 @@ const history = [];
  * @param {string} userMessage
  */
 function createUserMessage(userMessage){
-        
-
     if(dom.userMessageTemplate && dom.chatContainerEl){
-        console.log(dom.userMessageTemplate);
         const templateCopy = document.importNode(dom.userMessageTemplate.content, true);
         const textMessage = templateCopy.querySelector("p");
         textMessage.textContent = userMessage;
+        dom.chatContainerEl.appendChild(templateCopy);
+    }
+}
+function createAiMessage(aiMessage){
+    if(dom.aiMessageTemplate && dom.chatContainerEl){
+        const templateCopy = document.importNode(dom.aiMessageTemplate.content, true);
+        const textMessage = templateCopy.querySelector("p");
+        textMessage.textContent = aiMessage;
         dom.chatContainerEl.appendChild(templateCopy);
     }
 }
@@ -62,3 +67,4 @@ askClaude("Ciao Claude");
 */
 
 createUserMessage("Ciao");
+createAiMessage("Ciao");
